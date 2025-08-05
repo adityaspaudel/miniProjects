0953bc40237c15cd9af422d0c3678893ec54eb28
aaditya paudel <adityaspaudel@gmail.com>
2025-08-06 00:01:21 +0545

    dayofweek, dayofmonth and month added

--- client/src/app/digitalWatch/page.js
+++ client/src/app/digitalWatch/page.js
@@ -1,14 +1,19 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const DigitalWatch = () => {
   const [currentTime, setCurrentTime] = useState(new Date());
-  setTimeout(() => {
-    setCurrentTime(new Date());
-  }, 1000);
   const hours = currentTime.getHours();
   const minutes = currentTime.getMinutes();
   const seconds = currentTime.getSeconds();
+  const dayOfWeek = currentTime.getDay();
+  const dayOfMonth = currentTime.getDate();
+  const month = currentTime.getMonth();
+  useEffect(() => {
+    setTimeout(() => {
+      setCurrentTime(new Date());
+    }, 1000);
+  }, [seconds]);
 
   console.log(`${hours}:${minutes}:${seconds}`);
 
@@ -17,6 +22,9 @@ const DigitalWatch = () => {
       <div className="text-green-400">DigitalWatch</div>
       <div className="text-5xl rounded-2xl text-green-400 bg-gray-800 font-bold border-2 p-6 w-64">
         {hours}:{minutes}:{seconds}
+      </div>
+      <div className="text-red-400">
+        dayofweek:{dayOfWeek + 1}, month:{month + 1},dayofMonth:{dayOfMonth}
       </div>
     </div>
   );
