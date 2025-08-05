ba912b600d4da9f5d456123c60af8f73933abfc0
aaditya paudel <adityaspaudel@gmail.com>
2025-08-05 23:36:15 +0545

    screen height updated

--- client/src/app/digitalWatch/page.js
+++ client/src/app/digitalWatch/page.js
@@ -13,7 +13,7 @@ const DigitalWatch = () => {
   console.log(`${hours}:${minutes}:${seconds}`);
 
   return (
-    <div className="flex flex-col gap-2 items-center justify-center ">
+    <div className="flex flex-col gap-2 items-center justify-center bg-black h-screen">
       <div className="text-green-400">DigitalWatch</div>
       <div className="text-5xl rounded-2xl text-green-400 bg-gray-800 font-bold border-2 p-6 w-64">
         {hours}:{minutes}:{seconds}
