8e342fa95ec53e8d5a658b8986fab292dfe80111
aaditya paudel <adityaspaudel@gmail.com>
2025-08-08 11:28:05 +0545

    css update

--- client/src/app/bmiCalculator/page.js
+++ client/src/app/bmiCalculator/page.js
@@ -27,35 +27,48 @@ const BmiCalculator = () => {
     console.log(bmi);
   };
   return (
-    <div>
-      <div>BMI Calculator</div>
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          handleSubmit();
-        }}
-      >
-        <label htmlFor="weight">weight in kg</label>
-        <input
-          ref={weightRef}
-          type="number"
-          className="border-2"
-          min="10"
-          max="200"
-        />
-
-        <label htmlFor="height">Height in cm</label>
-        <input
-          ref={heightRef}
-          type="number"
-          className="border-2"
-          min="20"
-          max="250"
-        />
-        <button type="submit">submit</button>
-      </form>
-      <div className="flex flex-col gap-2">
-        <div>bmiValue: {bmiValue}</div> <div>bmiResult: {bmiResult}</div>
+    <div className="flex flex-col items-center justify-center gap-2 border-2 bg-amber-100">
+      <div className="flex flex-col items-center justify-center gap-2 p-8 border-2 border-green-400 bg-amber-100 rounded-xl">
+        <div className="text-4xl">BMI Calculator</div>
+        <hr className="w-64 border-gray-800 border-1" />
+        <form
+          className="flex flex-col items-start justify-center gap-2 "
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
+          <div>
+            <label htmlFor="weight">weight in kg</label>
+            <input
+              ref={weightRef}
+              type="number"
+              className="border-2 rounded-sm "
+              min="10"
+              max="200"
+            />
+          </div>
+          <div>
+            <label htmlFor="height">Height in cm</label>
+            <input
+              ref={heightRef}
+              type="number"
+              className="border-2 rounded-sm"
+              min="20"
+              max="250"
+            />
+          </div>
+          <button
+            type="submit "
+            className="w-16 p-2 text-sm bg-green-400 rounded-sm hover:bg-green-500"
+          >
+            submit
+          </button>
+        </form>
+        <div className="flex flex-col items-start justify-start gap-2 text-sm font-bold">
+          <div>bmiValue: {bmiValue}</div>
+          <div>bmiResult: {bmiResult}</div>
+        </div>
       </div>
     </div>
   );
