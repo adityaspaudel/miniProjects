a2ff62692d20aa287de72f339ddd948a4ea963e5
aaditya paudel <adityaspaudel@gmail.com>
2025-08-09 22:58:57 +0545

    css issue fixed

--- client/src/app/todoListApp/page.js
+++ client/src/app/todoListApp/page.js
@@ -10,18 +10,27 @@ const ToDoListApp = () => {
     setTodos((t) => [...t, inputRef.current.value]);
   };
   return (
-    <div>
-      <div>ToDoListApp</div>
-      <div>
-        <label htmlFor="todo"></label>
-        <input className="border-2" type="text" ref={inputRef} />
+    <div className=" bg-yellow-200 h-screen w-screen flex justify-center items-center">
+      <div className="bg-green-400 flex flex-col gap-2 justify-center items-center border-2 border-green-400 rounded-xl w-1/2 min-h-1/2 p-4">
+        <div className="text-4xl">ToDoListApp</div>
+        <hr className="w-full border-1" />
+        <div>
+          <label htmlFor="todo"></label>
+          <input
+            name="todo"
+            className="border-2 text-black"
+            type="text"
+            ref={inputRef}
+          />
+          <button onClick={addTodo}> ✚ add </button>
+        </div>
 
-        <button onClick={addTodo}>add todo</button>
+        <div className="flex flex-col font-mono justify-start items-start">
+          {todos.map((val, key) => (
+            <div key={key}>{val} </div>
+          ))}
+        </div>
       </div>
-
-      {todos.map((val, key) => (
-        <div key={key}>{val} </div>
-      ))}
     </div>
   );
 };
