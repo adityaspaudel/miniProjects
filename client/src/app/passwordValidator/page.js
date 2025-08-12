7549f6fe16489a95e54e66b4df3b3b1020a5792e
aaditya paudel <adityaspaudel@gmail.com>
2025-08-12 08:18:26 +0545

    reset button added, css updated

--- client/src/app/passwordValidator/page.js
+++ client/src/app/passwordValidator/page.js
@@ -37,22 +37,49 @@ const FormValidator = () => {
     alert("formSubmitted successfully", JSON.stringify(formData));
   };
   return (
-    <div>
-      <div>FormValidator</div>
-      <form onSubmit={handleSubmit}>
-        <label>email</label>
-        <input name="email" value={formData.email} onChange={handleChange} />
-        <div className="italic text-red-400">{errors.email}</div>
-        <label>password</label>
-        <input
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-        />
-        <div className="italic text-red-400">{errors.password}</div>
+    <div className="flex flex-col items-center justify-center gap-2 bg-amber-200">
+      <div className="flex flex-col items-center justify-center w-1/2 gap-2 p-4 bg-blue-300 border-2 rounded-lg h-1/2">
+        <div>FormValidator</div>
+        <form onSubmit={handleSubmit}>
+          <div className="flex flex-col">
+            <label>email</label>
+            <input
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              className="bg-white rounded-sm w-96"
+            />
+            <div className="italic text-red-400">{errors.email}</div>
+          </div>
 
-        <button type="submit">submit</button>
-      </form>
+          <div className="flex flex-col">
+            <label>password</label>
+            <input
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              className="bg-white rounded-sm w-96"
+            />
+            <div className="italic text-red-400">{errors.password}</div>
+          </div>
+          <br />
+          <div className="flex justify-between items-baseline-last ">
+            <button type="submit" className="px-2 bg-green-400">
+              submit
+            </button>
+            <button
+              className="px-2 bg-gray-400"
+              onClick={(e) => {
+                e.preventDefault();
+                setFormData({ email: "", password: "" });
+              }}
+            >
+              {" "}
+              reset
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
