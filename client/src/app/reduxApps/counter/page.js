03b865628842d61a3fc5826ca86f4b598bec396b
aaditya paudel <adityaspaudel@gmail.com>
2025-08-15 20:32:52 +0545

    css update

--- client/src/app/reduxApps/counter/page.js
+++ client/src/app/reduxApps/counter/page.js
@@ -9,16 +9,18 @@ export default function Counter() {
   const dispatch = useDispatch();
 
   return (
-    <div>
-      <div>
+    <div className="flex flex-col items-center justify-center text-black bg-amber-100">
+      <div className="flex flex-col items-center justify-center bg-red-400 border-2 p-4  border-blue-500 rounded-lg w-96">
         <button
+          className="border-2 border-black rounded-md px-2"
           aria-label="Increment value"
           onClick={() => dispatch(increment())}
         >
           Increment
         </button>
-        <span>{count}</span>
+        <span className="text-4xl">{count}</span>
         <button
+          className="border-2 border-black rounded-md px-2"
           aria-label="Decrement value"
           onClick={() => dispatch(decrement())}
         >
