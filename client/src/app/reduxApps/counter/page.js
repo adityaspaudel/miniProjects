3f0126476db0541642c1d8fc8a9bad1275fed0a5
aaditya paudel <adityaspaudel@gmail.com>
2025-08-20 22:15:21 +0545

    css update

--- client/src/app/reduxApps/counter/page.js
+++ client/src/app/reduxApps/counter/page.js
@@ -15,11 +15,11 @@ export default function Counter() {
 
   return (
     <div className="flex flex-col items-center justify-center text-black bg-amber-100 w-screen h-screen">
-      <div className="flex flex-col items-center justify-center bg-red-400 border-2 p-4  border-black rounded-lg w-1/2 h-1/2">
-        <div className="text-4xl">React-Redux Counter</div>
-        <hr className="border-1 border-black w-full" />
-        <br />
-        <br />
+      <div className="flex flex-col items-center gap-2 justify-center bg-red-400 border-2 p-4  border-black rounded-lg w-1/2 h-1/2">
+        <div>
+          <div className="text-4xl">React-Redux Counter</div>
+          <hr className="border-1 border-black w-full" />
+        </div>
         <button
           className="border-2 border-black rounded-md px-2"
           aria-label="Increment value"
@@ -27,9 +27,9 @@ export default function Counter() {
         >
           Increment
         </button>
-        <br />
+
         <span className="text-6xl text-black">{count}</span>
-        <br />
+
         <button
           className="border-2 border-black rounded-md px-2"
           aria-label="Decrement value"
@@ -38,10 +38,18 @@ export default function Counter() {
           Decrement
         </button>
 
-        <button onClick={() => dispatch(incrementByAmount(5))}>
+        <button
+          onClick={() => dispatch(incrementByAmount(5))}
+          className="border-2 border-black rounded-md px-2"
+        >
           Increment By 5
         </button>
-        <button onClick={() => dispatch(reset())}>reset</button>
+        <button
+          onClick={() => dispatch(reset())}
+          className="bg-gray-400 px-2 rounded-md border-2 border-red-300"
+        >
+          reset
+        </button>
       </div>
     </div>
   );
