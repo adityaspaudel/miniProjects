914008471a6bb58a283ccfb6225b3a2f87bc93ef
aaditya paudel <adityaspaudel@gmail.com>
2025-08-22 23:02:37 +0545

    css update in todos app

--- client/src/app/reduxApps/todosApp/page.js
+++ client/src/app/reduxApps/todosApp/page.js
@@ -17,29 +17,43 @@ export default function TodoApp() {
   };
 
   return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold">Todo List</h1>
-      <input
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Add todo"
-        className="border px-2 py-1 mr-2"
-      />
-      <button onClick={handleAdd}>Add</button>
-
-      <ul className="mt-4">
-        {todos.map((todo) => (
-          <li key={todo.id} className="flex justify-between items-center">
-            <span
-              onClick={() => dispatch(toggleTodo(todo.id))}
-              className={todo.done ? "line-through" : ""}
+    <div className="p-4 flex flex-col gap-2 justify-center items-center bg-slate-400 h-screen w-screen text-black">
+      <div className="flex flex-col gap-2 content-center items-center bg-lime-600 p-4 h-1/2 w-1/2 rounded-xl">
+        <div className="flex flex-col ">
+          <h1 className="text-4xl font-bold">Todo List</h1>
+          <hr className="w-full border-black" />
+        </div>
+        <div className="flex gap-2">
+          <input
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            placeholder="Add todo"
+            className="border px-2 py-1 mr-2"
+          />
+          <button
+            onClick={handleAdd}
+            className="bg-green-400 px-2 rounded-lg hover:bg-green-600"
+          >
+            Add
+          </button>
+        </div>
+        <ul className="mt-4">
+          {todos.map((todo) => (
+            <li
+              key={todo.id}
+              className="flex  justify-between content-between gap-10 items-center w-full"
             >
-              {todo.text}
-            </span>
-            <button onClick={() => dispatch(removeTodo(todo.id))}>❌</button>
-          </li>
-        ))}
-      </ul>
+              <div
+                onClick={() => dispatch(toggleTodo(todo.id))}
+                className={todo.done ? "line-through" : ""}
+              >
+                {todo.text}
+              </div>
+              <button onClick={() => dispatch(removeTodo(todo.id))}>❌</button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
