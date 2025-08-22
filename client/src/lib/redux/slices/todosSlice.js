37c50c59de810bc5e8021dd160ea2e83fe51d3cb
aaditya paudel <adityaspaudel@gmail.com>
2025-08-22 22:42:23 +0545

    todosSlice created and configured it to store

---
+++ client/src/lib/redux/slices/todosSlice.js
@@ -0,0 +1,21 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push({ id: Date.now(), text: action.payload, done: false });
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.find((t) => t.id === action.payload);
+      if (todo) todo.done = !todo.done;
+    },
+    removeTodo: (state, action) => {
+      return state.filter((t) => t.id !== action.payload);
+    },
+  },
+});
+
+export const { addTodo, toggleTodo, removeTodo } = todosSlice.actions;
+export default todosSlice.reducer;
--- client/src/lib/redux/store.js
+++ client/src/lib/redux/store.js
@@ -3,11 +3,13 @@ import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./slices/counterSlice"; // Example
 import cartReducer from "./slices/cartSlice";
 import themeReducer from "./slices/themeSlice";
+import todosReducer from ".slices/todosSlice";
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     cart: cartReducer,
     theme: themeReducer,
+    todos: todosReducer,
   },
 });
