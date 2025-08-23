ce337a19cae774330bc8498ce8b8582eef7363a6
aaditya paudel <adityaspaudel@gmail.com>
2025-08-23 21:40:14 +0545

    reducers combined in combineReducer in redux store

--- client/src/lib/redux/store.js
+++ client/src/lib/redux/store.js
@@ -1,15 +1,16 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import counterReducer from "./slices/counterSlice"; // Example
 import cartReducer from "./slices/cartSlice";
 import themeReducer from "./slices/themeSlice";
 import todosReducer from "./slices/todosSlice";
 
+const rootReducer = combineReducers({
+  counter: counterReducer,
+  cart: cartReducer,
+  theme: themeReducer,
+  todos: todosReducer,
+});
 export const store = configureStore({
-  reducer: {
-    counter: counterReducer,
-    cart: cartReducer,
-    theme: themeReducer,
-    todos: todosReducer,
-  },
+  reducer: rootReducer,
 });
