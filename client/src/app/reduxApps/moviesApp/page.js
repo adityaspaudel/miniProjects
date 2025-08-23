a6afe2f556c1a9749c48c6b909b141e0f281691f
aaditya paudel <adityaspaudel@gmail.com>
2025-08-23 22:45:14 +0545

    moviesApp created

---
+++ client/src/app/reduxApps/moviesApp/page.js
@@ -0,0 +1,66 @@
+"use client";
+
+import { useSelector, useDispatch } from "react-redux";
+import {
+  addFavorite,
+  removeFavorite,
+  clearFavorites,
+} from "@/lib/redux/slices/moviesSlice";
+
+export default function MoviesPage() {
+  const dispatch = useDispatch();
+  const favorites = useSelector((state) => state.movies.favorites);
+
+  // Sample movie list
+  const movies = [
+    { id: 1, title: "Chhakka panja", year: 2070 },
+    { id: 2, title: "Chor", year: 2071 },
+    { id: 3, title: "Sikari", year: 2072 },
+    { id: 4, title: "Chino", year: 2053 },
+    { id: 5, title: "Sanskar", year: 2058 },
+  ];
+
+  return (
+    <div>
+      <h1> Favorite Movies App</h1>
+
+      {/* Movies List */}
+      <h2>Movies</h2>
+      <div className="flex gap-2">
+        {movies.map((movie) => (
+          <button key={movie.id} onClick={() => dispatch(addFavorite(movie))}>
+            Add {movie.title}
+          </button>
+        ))}
+      </div>
+
+      {/* Favorites List */}
+      <h2>Favorites</h2>
+      {favorites.length === 0 ? (
+        <p>No favorite movies yet.</p>
+      ) : (
+        <ul>
+          {favorites.map((movie) => (
+            <li key={movie.id}>
+              <span>
+                {movie.title} :({movie.year})
+              </span>
+              <button onClick={() => dispatch(removeFavorite(movie.id))}>
+                Remove
+              </button>
+            </li>
+          ))}
+        </ul>
+      )}
+
+      {favorites.length > 0 && (
+        <button
+          onClick={() => dispatch(clearFavorites())}
+          className="mt-4 px-4 py-2 bg-gray-800 text-white rounded"
+        >
+          Clear All
+        </button>
+      )}
+    </div>
+  );
+}
