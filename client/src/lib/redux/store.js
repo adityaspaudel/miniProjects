f18f647fb58f65691f1ba86ea2e7d282b6a3781a
aaditya paudel <adityaspaudel@gmail.com>
2025-08-23 21:43:04 +0545

    notesSlice created

--- client/src/lib/redux/store.js
+++ client/src/lib/redux/store.js
@@ -4,12 +4,14 @@ import counterReducer from "./slices/counterSlice"; // Example
 import cartReducer from "./slices/cartSlice";
 import themeReducer from "./slices/themeSlice";
 import todosReducer from "./slices/todosSlice";
+import notesReducer from "./slices/notesSlice";
 
 const rootReducer = combineReducers({
   counter: counterReducer,
   cart: cartReducer,
   theme: themeReducer,
   todos: todosReducer,
+  notes: notesReducer,
 });
 export const store = configureStore({
   reducer: rootReducer,
