057b51c27051176ed705782441676a62299d9831
aaditya paudel <adityaspaudel@gmail.com>
2025-08-25 07:33:21 +0545

    css updated in useruth

--- client/src/app/reduxApps/userAuth/page.js
+++ client/src/app/reduxApps/userAuth/page.js
@@ -3,8 +3,6 @@
 import { login, logout } from "@/lib/redux/slices/authSlice";
 import { useDispatch, useSelector } from "react-redux";
 
-
-
 export default function UserLoginPage() {
   const dispatch = useDispatch();
   const { isLoggedIn, user } = useSelector((state) => state.auth);
@@ -20,18 +18,20 @@ export default function UserLoginPage() {
   };
 
   return (
-    <div>
-      {isLoggedIn ? (
-        <div>
-          <h2>Welcome, {user.name}!</h2>
-          <button onClick={handleLogout}>Logout</button>
-        </div>
-      ) : (
-        <div>
-          <h2>Please log in</h2>
-          <button onClick={handleLogin}>Login</button>
-        </div>
-      )}
+    <div className="flex flex-col gap-2 justify-center items-center bg-gray-500 h-screen w-screen">
+      <div className="flex flex-col gap-2 justify-center items-center bg-neutral-600 min-h-1/2 w-1/2 rounded-md p-4">
+        {isLoggedIn ? (
+          <div>
+            <h2>Welcome, {user.name}!</h2>
+            <button onClick={handleLogout}>Logout</button>
+          </div>
+        ) : (
+          <div>
+            <h2>Please log in</h2>
+            <button onClick={handleLogin}>Login</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
