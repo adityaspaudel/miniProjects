5fdc6f9ef57c96a82dc25b7384181be654c2c5ea
aaditya paudel <adityaspaudel@gmail.com>
2025-08-25 07:28:41 +0545

    userAuth page added

---
+++ client/src/app/reduxApps/userAuth/page.js
@@ -0,0 +1,37 @@
+"use client";
+
+import { login, logout } from "@/lib/redux/slices/authSlice";
+import { useDispatch, useSelector } from "react-redux";
+
+
+
+export default function UserLoginPage() {
+  const dispatch = useDispatch();
+  const { isLoggedIn, user } = useSelector((state) => state.auth);
+
+  const handleLogin = () => {
+    // In a real app, you would perform an API call here to authenticate the user
+    const userData = { name: "John Doe", email: "john.doe@example.com" };
+    dispatch(login(userData));
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
+  return (
+    <div>
+      {isLoggedIn ? (
+        <div>
+          <h2>Welcome, {user.name}!</h2>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+      ) : (
+        <div>
+          <h2>Please log in</h2>
+          <button onClick={handleLogin}>Login</button>
+        </div>
+      )}
+    </div>
+  );
+}
