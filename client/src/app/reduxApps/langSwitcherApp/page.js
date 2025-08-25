a139a5055cc7b705e92cf32ac974fffb0c92142d
aaditya paudel <adityaspaudel@gmail.com>
2025-08-25 12:53:36 +0545

    terinary logic values changed

--- client/src/app/reduxApps/langSwitcherApp/page.js
+++ client/src/app/reduxApps/langSwitcherApp/page.js
@@ -13,7 +13,12 @@ export default function LanguageSwitcher() {
 
   return (
     <div className="flex flex-col justify-center items-center bg-stone-400 h-screen w-screen text-black">
-      <div className="p-4 flex flex-col justify-center items-center bg-yellow-300 h-1/2 w-1/2">
+      <div className="p-4 flex flex-col content-center gap-2 items-center bg-yellow-300 h-1/2 w-1/2 rounded-xl">
+        <div>
+          <h1 className="text-4xl font-bold">Language Switcher App</h1>
+          <hr className="border-black" />
+        </div>
+
         <select
           value={language}
           onChange={handleLanguageChange}
@@ -23,11 +28,17 @@ export default function LanguageSwitcher() {
           <option value="np">Nepali</option>
           <option value="hi">Hindi</option>
         </select>
-        <p className="mt-4 text-lg font-semibold">
-          {language === "en" && "Hello, World!"}
-          {language === "np" && "नमस्ते, संसार!"}
-          {language === "hi" && "नमस्ते, दुनिया!"}
-        </p>
+        <div className="mt-4 text-lg font-semibold">
+          {language === "en" && (
+            <div>Hello, World! Welcome you to our language switcher app.</div>
+          )}
+          {language === "np" && (
+            <div>नमस्ते, संसार! हमारे भाषा स्विचर ऐप में आपका स्वागत है।</div>
+          )}
+          {language === "hi" && (
+            <div>नमस्ते, दुनिया! हाम्रो भाषा स्विचर एपमा स्वागत छ।</div>
+          )}
+        </div>
       </div>
     </div>
   );
