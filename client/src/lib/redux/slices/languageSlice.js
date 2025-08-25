4b47179563fd4dba70d38fbd01b869a430e95f36
aaditya paudel <adityaspaudel@gmail.com>
2025-08-25 12:33:38 +0545

    languageSlice.js created

---
+++ client/src/lib/redux/slices/languageSlice.js
@@ -0,0 +1,19 @@
+// src/lib/redux/slices/languageSlice.js
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  language: "en", // default language
+};
+
+const languageSlice = createSlice({
+  name: "language",
+  initialState,
+  reducers: {
+    setLanguage: (state, action) => {
+      state.language = action.payload;
+    },
+  },
+});
+
+export const { setLanguage } = languageSlice.actions;
+export default languageSlice.reducer;
