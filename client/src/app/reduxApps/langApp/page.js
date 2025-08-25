5240b3cf284ae79a40249407e4c74f751ec5c7a9
aaditya paudel <adityaspaudel@gmail.com>
2025-08-25 12:43:52 +0545

    css udated in langApp

--- client/src/app/reduxApps/langApp/page.js
+++ client/src/app/reduxApps/langApp/page.js
@@ -12,22 +12,23 @@ export default function LanguageSwitcher() {
   };
 
   return (
-    <div className="p-4">
-      <select
-        value={language}
-        onChange={handleLanguageChange}
-        className="border px-2 py-1 rounded"
-      >
-        <option value="en">English</option>
-        <option value="np">Nepali</option>
-        <option value="hi">Hindi</option>
-      </select>
-
-      <p className="mt-4 text-lg font-semibold">
-        {language === "en" && "Hello, World!"}
-        {language === "np" && "नमस्ते, संसार!"}
-        {language === "hi" && "नमस्ते, दुनिया!"}
-      </p>
+    <div className="flex flex-col justify-center items-center bg-stone-400 h-screen w-screen text-black">
+      <div className="p-4 flex flex-col justify-center items-center bg-yellow-300 h-1/2 w-1/2">
+        <select
+          value={language}
+          onChange={handleLanguageChange}
+          className="border px-2 py-1 rounded"
+        >
+          <option value="en">English</option>
+          <option value="np">Nepali</option>
+          <option value="hi">Hindi</option>
+        </select>
+        <p className="mt-4 text-lg font-semibold">
+          {language === "en" && "Hello, World!"}
+          {language === "np" && "नमस्ते, संसार!"}
+          {language === "hi" && "नमस्ते, दुनिया!"}
+        </p>
+      </div>
     </div>
   );
 }
