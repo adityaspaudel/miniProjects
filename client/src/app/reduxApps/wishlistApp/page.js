d8c3cbab566389ef0bbf0f5e6b63c73405dd7ffb
aaditya paudel <adityaspaudel@gmail.com>
2025-08-26 09:21:01 +0545

    css and UI updated

--- client/src/app/reduxApps/wishlistApp/page.js
+++ client/src/app/reduxApps/wishlistApp/page.js
@@ -15,20 +15,20 @@ export default function WishlistTest() {
 
   return (
     <div className="bg-slate-400 text-black flex flex-col gap-2 justify-center items-center h-screen w-screen">
-      <div className="bg-yellow-200 flex flex-col gap-2 min-h-1/2 w-1/2 content-center items-center">
+      <div className="bg-yellow-200 flex flex-col gap-2 min-h-1/2 w-1/2 content-center items-center rounded-xl">
         <div>
           <h2 className="text-4xl font-bold">My Wishlist</h2>
           <hr className="border-black border-1" />
         </div>
         <button
           onClick={() => dispatch(addToWishlist(sampleItem))}
-          className="bg-green-400 hover:bg-green-500 px-2"
+          className="bg-green-400 hover:bg-green-500 px-2 rounded-sm"
         >
           Add iPhone 16 Pro
         </button>
         <button
           onClick={() => dispatch(clearWishlist())}
-          className="bg-slate-500 text-red-400 px-2"
+          className="bg-slate-500 text-red-400 px-2 rounded-sm"
         >
           Clear Wishlist
         </button>
@@ -37,15 +37,22 @@ export default function WishlistTest() {
             <p>No items in wishlist</p>
           ) : (
             wishlist.map((item) => (
-              <li key={item.id} className=" flex gap-2">
-                {item.name}
-                <button
-                  onClick={() => dispatch(removeFromWishlist(item.id))}
-                  className="bg-red-400 hover:bg-red-500 px-2"
-                >
-                  Remove
-                </button>
-              </li>
+              <div key={item.id}>
+                {item && (
+                  <div>
+                    <div>Wishlist items</div>
+                    <li key={item.id} className=" flex gap-2 font-mono">
+                      {item.name}
+                      <button
+                        onClick={() => dispatch(removeFromWishlist(item.id))}
+                        className="bg-red-400 hover:bg-red-500 px-2 rounded-sm"
+                      >
+                        Remove
+                      </button>
+                    </li>
+                  </div>
+                )}
+              </div>
             ))
           )}
         </ul>
