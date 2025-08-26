ce6443f77402bc414a715283c3742d082fb15ea4
aaditya paudel <adityaspaudel@gmail.com>
2025-08-26 09:08:45 +0545

    UI updated

--- client/src/app/reduxApps/wishlistApp/page.js
+++ client/src/app/reduxApps/wishlistApp/page.js
@@ -14,43 +14,41 @@ export default function WishlistTest() {
   const sampleItem = { id: 1, name: "iPhone 16 Pro" };
 
   return (
-    <div >
-      <h2 >My Wishlist</h2>
-
-      <button
-        onClick={() => dispatch(addToWishlist(sampleItem))}
-       
-      >
-        Add iPhone 16 Pro
-      </button>
-
-      <button
-        onClick={() => dispatch(clearWishlist())}
-
-      >
-        Clear Wishlist
-      </button>
-
-      <ul className="mt-4">
-        {wishlist.length === 0 ? (
-          <p>No items in wishlist</p>
-        ) : (
-          wishlist.map((item) => (
-            <li
-              key={item.id}
-             
-            >
-              {item.name}
-              <button
-                onClick={() => dispatch(removeFromWishlist(item.id))}
-             
+    <div className="bg-slate-400 text-black flex flex-col gap-2 justify-center items-center h-screen w-screen">
+      <div className="bg-yellow-200 flex flex-col gap-2 min-h-1/2 w-1/2 content-center items-center">
+        <h2 >My Wishlist</h2>
+        <button
+          onClick={() => dispatch(addToWishlist(sampleItem))}
+        
+        >
+          Add iPhone 16 Pro
+        </button>
+        <button
+          onClick={() => dispatch(clearWishlist())}
+        >
+          Clear Wishlist
+        </button>
+        <ul className="mt-4">
+          {wishlist.length === 0 ? (
+            <p>No items in wishlist</p>
+          ) : (
+            wishlist.map((item) => (
+              <li
+                key={item.id}
+        
               >
-                Remove
-              </button>
-            </li>
-          ))
-        )}
-      </ul>
+                {item.name}
+                <button
+                  onClick={() => dispatch(removeFromWishlist(item.id))}
+        
+                >
+                  Remove
+                </button>
+              </li>
+            ))
+          )}
+        </ul>
+      </div>
     </div>
   );
 }
