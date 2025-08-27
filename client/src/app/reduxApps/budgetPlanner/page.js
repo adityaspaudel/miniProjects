8487c092c5977fdd72c3ef042cc4a9e524d37a10
aaditya paudel <adityaspaudel@gmail.com>
2025-08-27 10:55:36 +0545

    css update in budgetPlanner app

--- client/src/app/reduxApps/budgetPlanner/page.js
+++ client/src/app/reduxApps/budgetPlanner/page.js
@@ -39,32 +39,39 @@ export default function BudgetPlanner() {
 
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen bg-zinc-400 text-black">
-      <div className="flex flex-col justify-center items-center min-h-1/2 w-1/2 bg-red-300 rounded-lg">
+      <div className="flex flex-col justify-center items-center min-h-1/2 w-1/2 bg-red-300 rounded-lg p-4">
         <h1 className="text-2xl font-bold mb-4">💰 Budget Planner</h1>
         {/* Budget Setter */}
-        <div className="mb-4">
+        <div className="mb-4 flex gap-2">
           <input
+            className="px-2 rounded-sm"
             type="number"
             placeholder="Enter Budget"
             value={budgetInput}
             onChange={(e) => setBudgetInput(e.target.value)}
           />
-          <button onClick={handleSetBudget}>Set Budget</button>
+          <button
+            onClick={handleSetBudget}
+            className="bg-orange-400 hover:bg-orange-500 px-2 rounded-sm text-sm"
+          >
+            Set Budget
+          </button>
         </div>
         <div>
           <p>
-            Total Budget: <b>₹{budget}</b>
+            Total Budget: <b>Rs. {budget}</b>
           </p>
           <p className="text-lg">
-            Total Expenses: <b>₹{totalExpenses}</b>
+            Total Expenses: <b>Rs. {totalExpenses}</b>
           </p>
           <p className="text-lg">
-            Remaining: <b>₹{remaining}</b>
+            Remaining: <b>Rs. {remaining}</b>
           </p>
         </div>
         {/* Add Expense */}
-        <div>
+        <div className="flex gap-2">
           <input
+            className="px-2 rounded-sm font-mono"
             type="text"
             placeholder="Expense Name"
             value={expenseName}
@@ -72,27 +79,36 @@ export default function BudgetPlanner() {
           />
           <input
             type="number"
+            className="px-2 rounded-sm font-mono"
             placeholder="Amount"
             value={expenseAmount}
             onChange={(e) => setExpenseAmount(e.target.value)}
           />
-          <button onClick={handleAddExpense}>Add</button>
+          <button
+            onClick={handleAddExpense}
+            className="bg-green-400 hover:bg-green-500 px-2 roudned-sm text-sm"
+          >
+            Add
+          </button>
         </div>
         {/* Expense List */}
-        <ul className="mb-4">
+        <ul className="mb-4 p-4 flex flex-col gap-2">
           {expenses.map((exp) => (
-            <li key={exp.id}>
+            <li key={exp.id} className="flex gap-2 justify-between">
               <span>
                 {exp.name} - ₹{exp.amount}
               </span>
-              <button onClick={() => dispatch(removeExpense(exp.id))}>
+              <button
+                onClick={() => dispatch(removeExpense(exp.id))}
+                className="bg-red-400 hover:bg-red-400 text-sm rounded-sm font-mono px-2"
+              >
                 remove
               </button>
             </li>
           ))}
         </ul>
         {expenses.length > 0 && (
-          <button onClick={() => dispatch(clearExpenses())}>Clear All</button>
+          <button onClick={() => dispatch(clearExpenses())} className="bg-gray-200 hover:bg-gray-400 font-mono rounded-sm px-2">Clear All</button>
         )}
       </div>
     </div>
