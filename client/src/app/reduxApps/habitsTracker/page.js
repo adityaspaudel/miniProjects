9b01b70fe5a233fb5e47faa77c69ae9c897724e1
aaditya paudel <adityaspaudel@gmail.com>
2025-08-29 21:08:16 +0545

    css update in habitTracker app

--- client/src/app/reduxApps/habitsTracker/page.js
+++ client/src/app/reduxApps/habitsTracker/page.js
@@ -23,38 +23,40 @@ export default function HabitTracker() {
   };
 
   return (
-    <div className="text-black bg-slate-300">
-      <h2>Habit Tracker</h2>
-
-      <div>
-        <input
-          type="text"
-          value={habit}
-          placeholder="Enter a habit"
-          onChange={(e) => {
-            e.preventDefault();
-            setHabit(e.target.value);
-          }}
-        />
-        <button onClick={() => handleAdd()}>Add</button>
-        <button onClick={() => dispatch(clearHabits())}>Clear All</button>
+    <div className="text-black bg-slate-300 flex justify-center items-center h-screen w-screen">
+      <div className="text-black bg-orange-300 flex flex-col gap-2 min-h-1/2 w-1/2 content-center items-center ">
+        <h2>Habit Tracker</h2>
+        <div>
+          <input
+            type="text"
+            value={habit}
+            placeholder="Enter a habit"
+            onChange={(e) => {
+              e.preventDefault();
+              setHabit(e.target.value);
+            }}
+          />
+          <button onClick={() => handleAdd()}>Add</button>
+          <button onClick={() => dispatch(clearHabits())}>Clear All</button>
+        </div>
+        <ul>
+          {habits.map((h) => (
+            <li key={h.id}>
+              <span
+                onClick={() => dispatch(toggleHabit(h.id))}
+                className={` ${
+                  h.completed ? "line-through text-gray-500" : "no-underline"
+                }`}
+              >
+                {h.title}
+              </span>
+              <button onClick={() => dispatch(removeHabit(h.id))}>
+                remove
+              </button>
+            </li>
+          ))}
+        </ul>
       </div>
-
-      <ul>
-        {habits.map((h) => (
-          <li key={h.id}>
-            <span
-              onClick={() => dispatch(toggleHabit(h.id))}
-              className={` ${
-                h.completed ? "line-through text-gray-500" : "no-underline"
-              }`}
-            >
-              {h.title}
-            </span>
-            <button onClick={() => dispatch(removeHabit(h.id))}>remove</button>
-          </li>
-        ))}
-      </ul>
     </div>
   );
 }
