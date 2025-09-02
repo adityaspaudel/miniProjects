7e8901499aecdb3ce9f0e718601b23e073809d3f
aaditya paudel <adityaspaudel@gmail.com>
2025-09-02 18:04:06 +0545

    css update in weatherApp

--- client/src/app/reduxApps/weatherApp/page.js
+++ client/src/app/reduxApps/weatherApp/page.js
@@ -17,36 +17,38 @@ export default function WeatherApp() {
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen gap-6">
-      <h1 className="text-3xl font-bold">Weather App</h1>
-
-      <div className="flex gap-2">
-        <input
-          type="text"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-          placeholder="Enter city"
-          className="text-black px-4 py-2 border rounded-lg"
-        />
-        <button
-          onClick={handleSearch}
-          className="px-4 py-2 bg-blue-500 text-white rounded-lg"
-        >
-          Search
-        </button>
-      </div>
-
-      {loading && <p>Loading...</p>}
-      {error && <p className="text-red-500">{error}</p>}
-
-      {data && (
-        <div className="text-center mt-4">
-          <h2 className="text-2xl font-semibold">{data.name}</h2>
-          <p>Temperature: {data.main.temp}°C</p>
-          <p>Weather: {data.weather[0].description}</p>
-          <p>Humidity: {data.main.humidity}%</p>
+    <div className="flex justify-center items-center h-screen w-screen bg-slate-500 text-black">
+      <div className="flex flex-col items-center justify-center min-h-1/2 min-w-1/2 gap-6 bg-yellow-200  p-4 rounded-xl">
+        <div>
+          <h1 className="text-3xl font-bold">Weather App</h1>
+          <hr className="border-black" />
         </div>
-      )}
+        <div className="flex gap-2">
+          <input
+            type="text"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Enter city"
+            className="text-black px-4 py-2 border rounded-lg"
+          />
+          <button
+            onClick={handleSearch}
+            className="px-4 py-2 bg-blue-500 text-white rounded-lg"
+          >
+            Search
+          </button>
+        </div>
+        {loading && <p>Loading...</p>}
+        {error && <p className="text-red-500">{error}</p>}
+        {data && (
+          <div className="text-center mt-4">
+            <h2 className="text-2xl font-semibold">{data.name}</h2>
+            <p>Temperature: {data.main.temp}°C</p>
+            <p>Weather: {data.weather[0].description}</p>
+            <p>Humidity: {data.main.humidity}%</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
