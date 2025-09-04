c9a599ea91d6d5c926fca2056edf4e77a33332af
aaditya paudel <adityaspaudel@gmail.com>
2025-09-04 22:07:53 +0545

    css update in colorPickerApp

--- client/src/app/colorPickerApp/page.js
+++ client/src/app/colorPickerApp/page.js
@@ -7,15 +7,17 @@ export default function ColorPicker() {
   const [color, setColor] = useState("");
 
   return (
-    <div>
-      <h1 className="">Minimal Color Picker</h1>
-      <ChromePicker
-        color={color}
-        onChange={(color) => setColor(color.hex)}
-        disableAlpha={true}
-      />
-      <div className="mt-6 text-center">
-        <p className="mt-2 font-mono">{color}</p>
+    <div className="flex flex-col items-center justify-center gap-2 bg-slate-500 h-screen w-screen text-black">
+      <div className="flex flex-col gap-2 items-center content-center min-h/1/2 w-1/2 bg-yellow-300">
+        <h1 className=""> Color Picker</h1>
+        <ChromePicker
+          color={color}
+          onChange={(color) => setColor(color.hex)}
+          disableAlpha={true}
+        />
+        <div className="mt-6 text-center">
+          <p className="mt-2 font-mono">{color}</p>
+        </div>
       </div>
     </div>
   );
