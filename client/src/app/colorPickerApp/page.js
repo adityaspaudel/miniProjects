01ee46a913c0bb3d2067ebda037627add8c44df8
aaditya paudel <adityaspaudel@gmail.com>
2025-09-04 22:20:08 +0545

    css update in colorPiceked div

--- client/src/app/colorPickerApp/page.js
+++ client/src/app/colorPickerApp/page.js
@@ -7,20 +7,24 @@ export default function ColorPicker() {
   const [color, setColor] = useState("");
 
   return (
-    <div className="flex flex-col items-center justify-center gap-2 bg-slate-500 h-screen w-screen text-black">
+    <div className="flex flex-col items-center justify-center gap-4 bg-slate-500 h-screen w-screen text-black">
       <div className="flex flex-col gap-2 items-center content-center rounded-xl min-h/1/2 w-1/2 bg-yellow-300">
-        <h1 className=""> Color Picker</h1>
+        <h1 className="text-4xl font-bold">
+          {" "}
+          Color Picker
+          <hr className="border-black" />
+        </h1>
         <ChromePicker
           color={color}
           onChange={(color) => setColor(color.hex)}
           disableAlpha={true}
         />
         <div className="mt-6 text-center">
-          <div
-            style={{ backgroundColor:  color  }}
-            className="h-12 w-12"
-          ></div>
-          <p className="mt-2 font-mono">{color}</p>
+          <div style={{ backgroundColor: color }} className="h-12 w-12"></div>
+          <p className={`mt-2 font-mono `} 
+          >
+            {color}
+          </p>
         </div>
       </div>
     </div>
