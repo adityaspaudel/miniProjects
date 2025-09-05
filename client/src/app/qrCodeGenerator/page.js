1268daca13b5edf9215cdc6298a677212a3beda0
aaditya paudel <adityaspaudel@gmail.com>
2025-09-05 17:46:25 +0545

    css update in qrCodeGenerator app

--- client/src/app/qrCodeGenerator/page.js
+++ client/src/app/qrCodeGenerator/page.js
@@ -2,20 +2,23 @@
 
 import React, { useState } from "react";
 import { QRCodeCanvas } from "qrcode.react";
+
 export default function QRCodeGenerator() {
   const [text, setText] = useState("");
   return (
-    <div>
-      {" "}
-      <h2>QR Code Generator</h2>{" "}
-      <input
-        type="text"
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-        placeholder="Enter text or URL"
-        style={{ padding: "0.5rem", width: "250px" }}
-      />{" "}
-      {text && <QRCodeCanvas value={text} size={200} />}{" "}
+    <div className="flex flex-col justify-center items-center h-screen w-screen bg-slate-400">
+      <div className="flex flex-col gap-4 justify-center items-center">
+        {" "}
+        <h2>QR Code Generator</h2>{" "}
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder="Enter text or URL"
+          style={{ padding: "0.5rem", width: "250px" }}
+        />{" "}
+        {text && <QRCodeCanvas value={text} size={200} />}{" "}
+      </div>
     </div>
   );
 }
