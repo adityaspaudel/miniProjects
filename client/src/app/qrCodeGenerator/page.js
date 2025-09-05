1a62521288677ffa7e4fa0ee4c2b8d1c1709ca0a
aaditya paudel <adityaspaudel@gmail.com>
2025-09-05 21:22:25 +0545

    qrCodeGenerator changed into  userDetails Qr code generator

--- client/src/app/qrCodeGenerator/page.js
+++ client/src/app/qrCodeGenerator/page.js
@@ -3,32 +3,50 @@
 import React, { useState } from "react";
 import { QRCodeCanvas } from "qrcode.react";
 
-export default function QRCodeGenerator() {
-  const [text, setText] = useState("");
+export default function UserDetailsQRCodeGenerator() {
+  const [user, setUser] = useState({ name: "", email: "", phone: "" });
+
+  const qrValue = `Name: ${user.name}\nEmail: ${user.email}\nPhone: ${user.phone}`;
+
   return (
     <div className="flex flex-col justify-center items-center h-screen w-screen bg-slate-400 text-black">
-      <div className="flex flex-col gap-4 justify-center items-center min-h-1/2 w-1/2 bg-orange-300 p-4  rounded-xl">
+      <div className="flex flex-col gap-4 justify-center items-center min-h-1/2 w-1/2 bg-orange-300 p-4 rounded-xl">
         <h2 className="text-4xl font-bold text-black">
-          QR Code Generator <hr className="border-black" />
+          User Details QR Code <hr className="border-black" />
         </h2>
 
         <input
-          className="p-2 text-2xl rounded-lg"
+          className="p-2 text-xl rounded-lg w-full"
           type="text"
-          value={text}
-          onChange={(e) => setText(e.target.value)}
-          placeholder="Enter text or URL"
+          placeholder="Enter Name"
+          value={user.name}
+          onChange={(e) => setUser({ ...user, name: e.target.value })}
         />
-        {text && (
+        <input
+          className="p-2 text-xl rounded-lg w-full"
+          type="email"
+          placeholder="Enter Email"
+          value={user.email}
+          onChange={(e) => setUser({ ...user, email: e.target.value })}
+        />
+        <input
+          className="p-2 text-xl rounded-lg w-full"
+          type="number"
+          placeholder="Enter Phone"
+          value={user.phone}
+          onChange={(e) => setUser({ ...user, phone: e.target.value })}
+        />
+
+        {user.name || user.email || user.phone ? (
           <div className="flex flex-col gap-4 content-center items-center">
             <div className="font-bold">Your QR Code is here: </div>
-            <QRCodeCanvas value={text} size={300} />
+            <QRCodeCanvas value={qrValue} size={300} />
 
-            <div className="underline font-mono">
-              Scan to know the QRcode value
+            <div className="underline font-mono text-center">
+              Scan to see user details
             </div>
           </div>
-        )}
+        ) : null}
       </div>
     </div>
   );
