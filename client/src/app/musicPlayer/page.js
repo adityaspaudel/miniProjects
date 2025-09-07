b0acb3e28093219efc16ec791d31e01934709de6
aaditya paudel <adityaspaudel@gmail.com>
2025-09-07 14:54:41 +0545

    musicPlayer updated

--- client/src/app/musicPlayer/page.js
+++ client/src/app/musicPlayer/page.js
@@ -110,59 +110,66 @@ export default function JamendoMusicPlayer() {
 
   return (
     <div className="p-6 flex flex-col gap-4 justify-center items-center bg-gray-400 rounded-xl shadow-md  mx-auto text-black h-screen w-screen">
-      <h1 className="text-xl font-bold">🎵 Jamendo Music Player</h1>
-
-      {loading && <p>Loading tracks...</p>}
-
-      {currentTrack && (
-        <>
-          <img
-            src={currentTrack.image}
-            alt={currentTrack.title}
-            className="w-40 h-40 rounded-lg shadow"
-          />
-          <p className="font-semibold">{currentTrack.title}</p>
-          <p className="text-sm text-gray-600">By {currentTrack.artist}</p>
-
-          <audio ref={audioRef} src={currentTrack.url} />
-
-          <input
-            type="range"
-            value={progress}
-            onChange={handleSeek}
-            className="w-full"
-          />
-
-          <div className="flex gap-4 mt-2">
-            <button
-              onClick={prevTrack}
-              className="bg-gray-200 px-4 py-2 rounded-lg hover:bg-gray-500 transition"
-            >
-              ⏮ Previous
-            </button>
-            <button
-              onClick={togglePlay}
-              className="bg-blue-500 text-white  rounded-lg shadow hover:bg-blue-600 transition"
-            >
-              {isPlaying ? (
-                <span className="hover:bg-red-400 px-6 py-3 h-full w-full rounded-lg">
-                  ⏸ Pause
-                </span>
-              ) : (
-                <span className="hover:bg-green-400 h-full w-full px-6 py-3 rounded-lg">
-                  ▶ Play
-                </span>
-              )}
-            </button>
-            <button
-              onClick={nextTrack}
-              className="bg-gray-200 px-4 py-2 rounded-lg hover:bg-gray-500 transition"
-            >
-              ⏭ Next
-            </button>
+      <div className="flex flex-col gap-4 items-center content-center bg-stone-400 rounded-xl p-4 min-h-1/2 w-1/2">
+        <h1 className="text-4xl font-bold flex">
+          🎵
+          <div>
+            Jamendo Music Player
+            <hr className="border-black" />
           </div>
-        </>
-      )}
+        </h1>
+        {loading && <p>Loading tracks...</p>}
+        {currentTrack && (
+          <>
+            <img
+              src={currentTrack.image}
+              alt={currentTrack.title}
+              className="w-40 h-40 rounded-lg shadow"
+            />
+            <p className="font-semibold" title="song">
+              {currentTrack.title}
+            </p>
+            <p className="text-sm text-yellow-800" title="artist">
+              By {currentTrack.artist}
+            </p>
+            <audio ref={audioRef} src={currentTrack.url} />
+            <input
+              type="range"
+              value={progress}
+              onChange={handleSeek}
+              className="w-full"
+            />
+            <div className="flex gap-4 mt-2">
+              <button
+                onClick={prevTrack}
+                className="bg-gray-200 px-4 py-2 rounded-lg hover:bg-gray-500 transition"
+              >
+                ⏮ Previous
+              </button>
+              <button
+                onClick={togglePlay}
+                className="bg-blue-500 text-white  rounded-lg shadow hover:bg-blue-600 transition"
+              >
+                {isPlaying ? (
+                  <span className="hover:bg-red-400 px-6 py-3 h-full w-full rounded-lg">
+                    ⏸ Pause
+                  </span>
+                ) : (
+                  <span className="hover:bg-green-400 h-full w-full px-6 py-3 rounded-lg">
+                    ▶ Play
+                  </span>
+                )}
+              </button>
+              <button
+                onClick={nextTrack}
+                className="bg-gray-200 px-4 py-2 rounded-lg hover:bg-gray-500 transition"
+              >
+                ⏭ Next
+              </button>
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 }
