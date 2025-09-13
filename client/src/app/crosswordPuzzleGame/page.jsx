d84c6795399e3456a5fd9cd1d303e41b4b76791e
aaditya paudel <adityaspaudel@gmail.com>
2025-09-13 16:24:29 +0545

    input update in crossPuzzlegame

--- client/src/app/crosswordPuzzleGame/page.jsx
+++ client/src/app/crosswordPuzzleGame/page.jsx
@@ -1,14 +1,8 @@
 "use client";
 
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 const CrosswordPuzzleGame = () => {
-  const [revealedHints, setRevealedHints] = useState([
-    ["", "a", "t", ""],
-    ["", "r", "a", ""],
-    ["b", "", "", "d"],
-    ["", "o", "x", ""],
-  ]);
   const defaultAnswer = [
     ["c", "a", "t", ""],
     ["", "r", "a", "t"],
@@ -16,21 +10,38 @@ const CrosswordPuzzleGame = () => {
     ["f", "o", "x", ""],
   ];
 
+  const inputRef1 = useRef([
+    ["", "a", "t", ""],
+    ["", "r", "a", ""],
+    ["b", "", "", "d"],
+    ["", "o", "x", ""],
+  ]);
+
+  // dummy state to trigger re-render after updates
+  const [tick, setTick] = useState(0);
+
+  const handleChange = (row, col, e) => {
+    inputRef1.current[row][col] = e.target.value.toLowerCase();
+    setTick((t) => t + 1); // force re-render to update UI
+  };
+
   return (
-    <div>
+    <div className="bg-slate-300 text-black p-4">
+      <div>Crossword Puzzle Game</div>
       <div>
-        <div>CrosswordPuzzleGame</div>
-        <div>
-          {revealedHints.map((val, key) => (
-            <div key={key}>
-              <div className="flex">
-                {val.map((v, k) => (
-                  <div key={k}>{v}</div>
-                ))}
-              </div>
-            </div>
-          ))}
-        </div>
+        {inputRef1.current.map((row, rowIndex) => (
+          <div key={rowIndex} className="flex">
+            {row.map((cell, colIndex) => (
+              <input
+                key={colIndex}
+                maxlength="1"
+                className="w-20 h-20 text-4xl text-center border"
+                value={cell}
+                onChange={(e) => handleChange(rowIndex, colIndex, e)}
+              />
+            ))}
+          </div>
+        ))}
       </div>
     </div>
   );
