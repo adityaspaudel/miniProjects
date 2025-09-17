2c2a1a26493874d2f13ee55899d58d94e465ddb5
aaditya paudel <adityaspaudel@gmail.com>
2025-09-17 20:42:02 +0545

    css update in dynamicUsersProfile

--- client/src/app/apiProjects/dynamicUsersProfile/page.js
+++ client/src/app/apiProjects/dynamicUsersProfile/page.js
@@ -9,17 +9,21 @@ export default async function Home() {
   const users = await getUsers();
 
   return (
-    <div>
-      <h1>Dynamic Users (App Router)</h1>
-      <ul>
-        {users.map((user) => (
-          <li key={user.id}>
-            <Link href={`/apiProjects/dynamicUsersProfile/users/${user.id}`}>
-              {user.name}
-            </Link>
-          </li>
-        ))}
-      </ul>
+    <div className="flex justify-center bg-slate-200 h-screen w-screen text-black">
+      <div className="flex flex-col content-center items-center gap-2 min-h-1/2 w-1/2 bg-emerald-200 p-4 rounded-xl">
+        <h1 className="text-4xl font-bold">
+          Dynamic Users (App Router) <hr className="border-black" />
+        </h1>
+        <ul>
+          {users.map((user) => (
+            <li key={user.id} className="hover:text-blue-400">
+              <Link href={`/apiProjects/dynamicUsersProfile/users/${user.id}`}>
+                {user.name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- client/src/app/apiProjects/dynamicUsersProfile/users/[id]/page.js
+++ client/src/app/apiProjects/dynamicUsersProfile/users/[id]/page.js
@@ -10,19 +10,27 @@ export default async function UserDetail({ params }) {
   const user = await getUser(id);
 
   return (
-    <div>
-      <h1>{user.name}</h1>
-      <p>
-        <strong>Email:</strong> {user.email}
-      </p>
-      <p>
-        <strong>Phone:</strong> {user.phone}
-      </p>
-      <p>
-        <strong>Website:</strong> {user.website}
-      </p>
-
-      <Link href="/apiProjects/dynamicUsersProfile">⬅ Back to Users</Link>
+    <div className="bg-slate-300 h-screen w-screen text-black flex justify-center items-center">
+      <div className="flex flex-col gap-2 justify-center items-center bg-emerald-200 rounded-xl min-h-1/2 w-1/2 p-4">
+        <h1>
+          <strong>
+            {user.name}
+            <hr className="border-black" />
+          </strong>
+        </h1>
+        <p>
+          <strong>Email:</strong> {user.email}
+        </p>
+        <p>
+          <strong>Phone:</strong> {user.phone}
+        </p>
+        <p>
+          <strong>Website:</strong> {user.website}
+        </p>
+        <div className="hover:text-blue-600">
+          <Link href="/apiProjects/dynamicUsersProfile">⬅ Back to Users</Link>
+        </div>
+      </div>
     </div>
   );
 }
