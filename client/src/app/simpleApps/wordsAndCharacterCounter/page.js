53e6b4b5aa9dc34bf8d3cc535204f43133a388f9
aaditya paudel <adityaspaudel@gmail.com>
2025-09-17 13:49:47 +0545

    css Updated in wordsAndCharacters counter app

--- client/src/app/simpleApps/wordsAndCharacterCounter/page.js
+++ client/src/app/simpleApps/wordsAndCharacterCounter/page.js
@@ -14,22 +14,26 @@ export default function WordAndCharacterCounter() {
   const wordCount = text.trim() === "" ? 0 : text.trim().split(/\s+/).length;
 
   return (
-    <div>
-      <h1>Word & Character Counter</h1>
-
-      <textarea
-        value={text}
-        onChange={handleChange}
-        maxLength={maxLength}
-        placeholder="Type your text here..."
-        rows={5}
-        cols={40}
-      />
-
-      <div>
-        Characters: {text.length} / {maxLength}
+    <div className="bg-slate-300 h-screen w-screen flex justify-center items-center">
+      <div className="flex flex-col gap-4 content-center items-center bg-emerald-200 min-h-1/2 w-1/2 p-4 rounded-xl">
+        <h1 className="text-4xl font-bold">
+          Word & Character Counter
+          <hr className="border-black" />
+        </h1>
+        <textarea
+          className="bg-white hover:border-black rounded-sm p-2"
+          value={text}
+          onChange={handleChange}
+          maxLength={maxLength}
+          placeholder="Type your text here..."
+          rows={5}
+          cols={40}
+        />
+        <div>
+          Characters: {text.length} / {maxLength}
+        </div>
+        <div>Words: {wordCount}</div>
       </div>
-      <div>Words: {wordCount}</div>
     </div>
   );
 }
