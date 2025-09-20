47b44751e29e650f55edc0b137e7a7f427484e9c
aaditya paudel <adityaspaudel@gmail.com>
2025-09-20 16:41:15 +0545

    user registration and login models, controllers,and routes created successfully

--- server/src/index.js
+++ server/src/index.js
@@ -5,11 +5,16 @@ const connectDB = require("./db/db.js");
 const dotenv = require("dotenv");
 dotenv.config();
 
+const userRoutes = require("./routes/userRoute");
+
 // middleware
 app.use(cors());
 app.use(express.json());
 
 connectDB();
+
+app.use("/api/users", userRoutes);
+
 
 // application
 const PORT = process.env.PORT || 8000;
---
+++ server/src/models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+//  User Schema
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+// user model
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
