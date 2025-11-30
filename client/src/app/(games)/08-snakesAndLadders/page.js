d3584bb1392affe6c609957cdff8db04e2c81733
Aaditya Paudel <adityaspaudel@gmail.com>
2025-11-30 23:00:31 +0545

    snakes and ladders update

--- client/src/app/(games)/08-snakesAndLadders/page.js
+++ client/src/app/(games)/08-snakesAndLadders/page.js
@@ -76,7 +76,7 @@ const SnakeAndLadderGame = () => {
   };
 
   return (
-    <div className="flex flex-col gap-4 items-center py-6">
+    <div className="flex flex-col gap-4 items-center py-6 bg-gray-100 text-black">
       <h1 className="text-4xl font-bold">Snakes And Ladders Game</h1>
 
       <div className="flex gap-4">
