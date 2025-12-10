d2c123e4bc9dea344ede9c7c988d1bdc4e284f40
Aaditya Paudel <adityaspaudel@gmail.com>
2025-12-10 15:37:50 +0545

    directory name updated from simpleApps to utilityApps, and css updated

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -3,122 +3,122 @@ import Link from "next/link";
 import React from "react";
 
 const Home = () => {
-	const games = [
-		"1-coinFlipper",
-		"2-crosswordPuzzleGame",
-		"3-hangman",
-		"4-loveCalculator",
-		"5-numberGuessingGame",
-		"6-quizBattleGame",
-		"7-rockPaperScissors",
-		"8-snakesAndLadders",
-		"9-typingSpeedTestGame",
-		"10-memoryCardGame",
-		"11-ticTacToe",
-		"12-snakeGame",
-		"13-aimTrainingGame",
-		"14-wordPuzzleGame",
-		"15-jumpingGame",
-		"16-brickBreaker",
-		"17-racingGame",
-		"18-ticTacToe",
-	];
+  const games = [
+    "1-coinFlipper",
+    "2-crosswordPuzzleGame",
+    "3-hangman",
+    "4-loveCalculator",
+    "5-numberGuessingGame",
+    "6-quizBattleGame",
+    "7-rockPaperScissors",
+    "8-snakesAndLadders",
+    "9-typingSpeedTestGame",
+    "10-memoryCardGame",
+    "11-ticTacToe",
+    "12-snakeGame",
+    "13-aimTrainingGame",
+    "14-wordPuzzleGame",
+    "15-jumpingGame",
+    "16-brickBreaker",
+    "17-racingGame",
+    "18-ticTacToe-v2",
+  ];
 
-	const reduxApps = [
-		"1-counter",
-		"2-notesApp",
-		"3-themeToggle",
-		"4-userAuth",
-		"5-todosApp",
-		"6-langSwitcherApp",
-		"7-habitsTracker",
-		"8-budgetPlanner",
-		"9-expenseTracker",
-		"10-inventoryManagementApp",
-		"11-moviesApp",
-		"12-notificationSystem",
-		"13-readingListApp",
-		"14-reduxCart",
-		"15-weatherApp",
-		"16-wishlistApp",
-		"17-githubUserFinder",
-	];
+  const reduxApps = [
+    "1-counter",
+    "2-notesApp",
+    "3-themeToggle",
+    "4-userAuth",
+    "5-todosApp",
+    "6-langSwitcherApp",
+    "7-habitsTracker",
+    "8-budgetPlanner",
+    "9-expenseTracker",
+    "10-inventoryManagementApp",
+    "11-moviesApp",
+    "12-notificationSystem",
+    "13-readingListApp",
+    "14-reduxCart",
+    "15-weatherApp",
+    "16-wishlistApp",
+    "17-githubUserFinder",
+  ];
 
-	const simpleApps = [
-		"1-accordion",
-		"2-bmiCalculator",
-		"3-calculator",
-		"4-colorPickerApp",
-		"5-counter",
-		"6-currencyConverter",
-		"7-digitalWatch",
-		"8-formValidator",
-		"9-matrixGenerator",
-		"10-musicPlayer",
-		"11-passwordGenerator",
-		"12-passwordValidator",
-		"13-pdfRenderer",
-		"14-photoGallery",
-		"15-pomodoroTimer",
-		"16-qrCodeGenerator",
-		"17-randomJokesGenerator",
-		"18-reminderApp",
-		"19-sidebarToggle",
-		"20-themeToggler",
-		"21-toDosListApp",
-		"22-unitConverter",
-		"23-websiteSurveys",
-		"24-wordsAndCharactersCounter",
-		"25-paginationWithAPI",
-		"26-urlShortener",
-		"27-calendarEventCreator",
-	];
+  const utilityApps = [
+    "1-accordion",
+    "2-bmiCalculator",
+    "3-calculator",
+    "4-colorPickerApp",
+    "5-counter",
+    "6-currencyConverter",
+    "7-digitalWatch",
+    "8-formValidator",
+    "9-matrixGenerator",
+    "10-musicPlayer",
+    "11-passwordGenerator",
+    "12-passwordValidator",
+    "13-pdfRenderer",
+    "14-photoGallery",
+    "15-pomodoroTimer",
+    "16-qrCodeGenerator",
+    "17-randomJokesGenerator",
+    "18-reminderApp",
+    "19-sidebarToggle",
+    "20-themeToggler",
+    "21-toDosListApp",
+    "22-unitConverter",
+    "23-websiteSurveys",
+    "24-wordsAndCharactersCounter",
+    "25-paginationWithAPI",
+    "26-urlShortener",
+    "27-calendarEventCreator",
+  ];
 
-	const renderCards = (items, basePath) =>
-		items.map((item, idx) => (
-			<Link href={`/${basePath}/${item}`} key={idx}>
-				<div className="bg-white shadow-md rounded-lg p-4 hover:scale-105 hover:shadow-xl transition-transform cursor-pointer text-center">
-					{item.split("-")[1]}
-				</div>
-			</Link>
-		));
+  const renderCards = (items, basePath) =>
+    items.map((item, idx) => (
+      <Link href={`/${basePath}/${item}`} key={idx}>
+        <div className="bg-gray-100 hover:font-semibold hover:bg-gray-300 shadow-gray-400 shadow rounded-lg px-8 py-4 hover:scale-105 hover:shadow-md transition-transform cursor-pointer ">
+          {item}
+        </div>
+      </Link>
+    ));
 
-	return (
-		<div className="min-h-screen text-black bg-gray-100 p-6">
-			<h1 className="text-4xl font-bold mb-8 text-center text-gray-800">
-				React Projects Hub
-			</h1>
+  return (
+    <div className="min-h-screen text-black bg-cyan-200 p-6">
+      <h1 className="text-4xl font-bold mb-8 text-center text-black">
+        React Projects Hub
+      </h1>
 
-			<div className="flex flex-col md:flex-row md:justify-between gap-8">
-				<div className="flex-1">
-					<h2 className="text-2xl font-semibold mb-4 text-gray-700 text-center">
-						Games
-					</h2>
-					<div className="flex flex-col flex-wrap gap-4 justify-center">
-						{renderCards(games, "games")}
-					</div>
-				</div>
+      <div className="flex flex-col md:flex-row md:justify-between gap-8">
+        <div className="flex-1">
+          <h2 className="text-2xl font-semibold mb-4 text-black text-center">
+            Games
+          </h2>
+          <div className="flex flex-col flex-wrap gap-4 justify-center">
+            {renderCards(games, "games")}
+          </div>
+        </div>
 
-				<div className="flex-1">
-					<h2 className="text-2xl font-semibold mb-4 text-gray-700 text-center">
-						Redux Apps
-					</h2>
-					<div className="flex flex-col flex-wrap gap-4 justify-center">
-						{renderCards(reduxApps, "reduxApps")}
-					</div>
-				</div>
+        <div className="flex-1">
+          <h2 className="text-2xl font-semibold mb-4 text-black text-center">
+            Redux Apps
+          </h2>
+          <div className="flex flex-col flex-wrap gap-4 justify-center">
+            {renderCards(reduxApps, "reduxApps")}
+          </div>
+        </div>
 
-				<div className="flex-1">
-					<h2 className="text-2xl font-semibold mb-4 text-gray-700 text-center">
-						Simple Apps
-					</h2>
-					<div className="flex flex-col flex-wrap gap-4 justify-center">
-						{renderCards(simpleApps, "simpleApps")}
-					</div>
-				</div>
-			</div>
-		</div>
-	);
+        <div className="flex-1">
+          <h2 className="text-2xl font-semibold mb-4 text-black text-center">
+            Utility Apps
+          </h2>
+          <div className="flex flex-col flex-wrap gap-4 justify-center">
+            {renderCards(utilityApps, "utilityApps")}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
