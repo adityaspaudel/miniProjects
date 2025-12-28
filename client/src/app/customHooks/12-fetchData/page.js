7bdcc312900493b2ef7208d68e534de3bf1bd5a3
Aaditya Paudel <adityaspaudel@gmail.com>
2025-12-28 19:09:47 +0545

    fetchData customHook app update

--- client/src/app/customHooks/12-fetchData/page.js
+++ client/src/app/customHooks/12-fetchData/page.js
@@ -2,7 +2,7 @@ import Users from "./Users";
 
 export default function HomePage() {
 	return (
-		<main style={{ padding: "20px" }}>
+		<main style={{ padding: "20px" }} className="flex bg-gray-200">
 			<h1>Custom API Hook – Flat App Folder</h1>
 			<Users />
 		</main>
