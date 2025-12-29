8fec8b9621bbff63ad1a303f5692469cb176b4fb
Aaditya Paudel <adityaspaudel@gmail.com>
2025-12-29 17:27:09 +0545

    useLayoutEffectTabs app created

--- client/src/app/customHooks/12-fetchData/page.js
+++ client/src/app/customHooks/12-fetchData/page.js
@@ -2,7 +2,10 @@ import Users from "./Users";
 
 export default function HomePage() {
 	return (
-		<main style={{ padding: "20px" }} className="flex bg-gray-200">
+		<main
+			style={{ padding: "20px" }}
+			className="flex bg-gray-200 h-screen w-screen"
+		>
 			<h1>Custom API Hook – Flat App Folder</h1>
 			<Users />
 		</main>
